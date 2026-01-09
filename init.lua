700bb961d9662d3eefaf76e4fcc31a45c5de68b0
Paul <paul@pop-os.schaiklan>
2026-01-09 23:30:43 +0100

    Initial commit, starting basic config

---
+++ init.lua
@@ -0,0 +1,38 @@
+-- nvim lua config Paul 2026
+-- loading lazy plugin manager
+local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
+  local out = vim.fn.system { 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath }
+  if vim.v.shell_error ~= 0 then
+    error('Error cloning lazy.nvim:\n' .. out)
+  end
+end
+
+local rtp = vim.opt.rtp
+rtp:prepend(lazypath)
+
+-- loading plugins
+require('lazy').setup({
+-- Neotree 
+{
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "MunifTanjim/nui.nvim",
+      "nvim-tree/nvim-web-devicons", -- optional, but recommended
+    },
+    lazy = false, -- neo-tree will lazily load itself
+ },
+-- Nightfox / Carbonfox theme 
+ {
+    "EdenEast/nightfox.nvim",
+    lazy = false
+ }
+})	
+
+-- loading settings
+require 'settings.core'
+require 'settings.keymaps'
+
